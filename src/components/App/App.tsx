c9fa6ce7a28c97bd61b6f2abac3c52c5a12b830f
trsaints <thiago.dev2k3@gmail.com>
2024-06-26 23:17:02 -0300

    feat: add main navbar to root content

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { Router } from '../Router/Router'
 import { Index } from './Index/Index'
 import { MyTasks } from './MyTasks/MyTasks'
+import { Navbar } from '../Navbar/Navbar'
 
 function App() {
     const routes = {
@@ -10,12 +11,17 @@ function App() {
         elements: [Index.Root(), MyTasks.Root()],
     }
 
+    const links = {
+        placeholders: ['Home', 'My Tasks'],
+        urls: ['/', '/my-tasks'],
+    }
+
     return (
-        <>
-            <BrowserRouter>
-                <Router.Root paths={routes.paths} elements={routes.elements} />
-            </BrowserRouter>
-        </>
+        <BrowserRouter>
+            <Navbar.Root {...links} />
+
+            <Router.Root {...routes} />
+        </BrowserRouter>
     )
 }
 
