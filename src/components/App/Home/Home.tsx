ab542fd073c6b2f197a5cac440d45445fec401db
trsaints <thiago.dev2k3@gmail.com>
2024-06-28 16:50:45 -0300

    update: test Content component

--- src/components/App/Home/Home.tsx
+++ src/components/App/Home/Home.tsx
@@ -1,10 +1,15 @@
+import { Content } from '../../Content'
 import { Heading } from '../../Heading'
 import { Section } from '../../Section'
 
 function Root() {
     return (
         <Section.Root block="home">
-            <Heading.Root>Home</Heading.Root>
+            <Heading.Root>Início</Heading.Root>
+
+            <Content.Root>
+                <Heading.Root>Opções</Heading.Root>
+            </Content.Root>
         </Section.Root>
     )
 }
