df4c8f817698326061b96e0a8e9562b45e44a906
trsaints <thiago.dev2k3@gmail.com>
2024-06-27 22:06:22 -0300

    feat: create Heading component

---
+++ src/Props/IHeadingProps.ts
@@ -0,0 +1,3 @@
+export default interface IHeadingProps {
+    children: React.ReactNode
+}
---
+++ src/components/Heading/Heading.tsx
@@ -0,0 +1,14 @@
+import { useContext } from "react"
+import { SectionContext } from "../../context/SectionContext"
+import IHeadingProps from "../../Props/IHeadingProps"
+
+function Root(props: IHeadingProps) {
+    const context = useContext(SectionContext)
+    const blockElement = `${context.block}__heading` 
+
+    return <h1 className={blockElement}>{props.children}</h1>
+}
+
+export const Heading = {
+    Root,
+}
