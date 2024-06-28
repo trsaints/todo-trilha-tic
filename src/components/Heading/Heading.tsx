96feb14e1e8733fa1ba0d46cd921739faa9395bc
trsaints <thiago.dev2k3@gmail.com>
2024-06-28 16:50:03 -0300

    feat: adapt semantic to given nesting level

--- src/components/Heading/Heading.tsx
+++ src/components/Heading/Heading.tsx
@@ -6,7 +6,22 @@ function Root(props: IHeadingProps) {
     const context = useContext(SectionContext)
     const blockElement = `${context.block}__heading`
 
-    return <h1 className={blockElement}>{props.children}</h1>
+    switch (context.level) {
+        case 1:
+            return <h1 className={blockElement}>{props.children}</h1>
+        case 2:
+            return <h2 className={blockElement}>{props.children}</h2>
+        case 3:
+            return <h3 className={blockElement}>{props.children}</h3>
+        case 4:
+            return <h4 className={blockElement}>{props.children}</h4>
+        case 5:
+            return <h5 className={blockElement}>{props.children}</h5>
+        case 6:
+            return <h6 className={blockElement}>{props.children}</h6>
+        default:
+            return <hgroup className={blockElement}>{props.children}</hgroup>
+    }
 }
 
 export const Heading = {
