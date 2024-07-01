2ad83d6e4c1bf1f57db06db11bab23de93f60374
trsaints <thiago.dev2k3@gmail.com>
2024-07-01 17:40:49 -0300

    feat: create Task global type

---
+++ src/Types/Task.ts
@@ -0,0 +1,10 @@
+type Priority = 'low' | 'medium' | 'high'
+
+export type Task = {
+	id: number
+	title: string
+	priority: Priority
+	completionDate: Date
+	creationDate: Date
+	description: string
+}
