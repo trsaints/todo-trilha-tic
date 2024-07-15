b0f1322502bb9bbc02a410095415c4811876870b
trsaints <thiago.dev2k3@gmail.com>
2024-07-15 10:01:53 -0300

    feat: create handler signatures for performing other read-write operations upon ITask

--- src/context/models/TasksPageContext/ITasksPageContext.ts
+++ src/context/models/TasksPageContext/ITasksPageContext.ts
@@ -1,7 +1,11 @@
 import {FormEventHandler, MouseEventHandler} from 'react'
+import {ITask} from '../../../entities'
 
 export interface ITasksPageContext {
     saveTaskTemporarily: FormEventHandler<HTMLFormElement>
     createTask: FormEventHandler<HTMLFormElement>
+    deleteTask: (taskId: string) => void
+    updateTask: (taskId: string,  newTask: ITask) => void 
     openTaskForm: MouseEventHandler<HTMLButtonElement>
+    handleTaskOptions: MouseEventHandler<HTMLElement>
 }
