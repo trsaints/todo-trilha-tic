92c7b6861255f6cf12a5d147ea3f916d1b95908b
trsaints <thiago.dev2k3@gmail.com>
2024-07-16 17:06:46 -0300

    feat: create CRUD operation methods upon local database

--- src/services/task-service.ts
+++ src/services/task-service.ts
@@ -1,5 +1,45 @@
 import {ITask} from '../entities'
 import {Priority} from '../types'
+import {mockApi} from './mock-api.ts'
+
+async function getTasks() {
+    const {data = []} = await mockApi.get('http://localhost:3333/tasks')
+
+    return data.map((task: ITask) => {
+        return {
+            ...task,
+            creationDate: new Date(task.creationDate),
+            completionDate: new Date(task.completionDate),
+        }
+    }) as ITask[]
+}
+
+async function addTask(task: ITask) {
+    const parsedTask = {
+        ...task, 
+        id: task.id.toString(),
+    }
+    const {data} = await mockApi.post('http://localhost:3333/tasks', parsedTask)
+
+    return {
+        ...data, 
+        creationDate: new Date(data.creationDate),
+        completionDate: new Date(data.completionDate),
+    }
+}
+
+async function updateTask(task: ITask) {
+    const parsedTask = {
+        ...task, 
+        id: task.id.toString(),
+    }
+    
+    await mockApi.put(`http://localhost:3333/tasks/${task.id}`, parsedTask)
+}
+
+async function removeTask(id: string) {
+    await mockApi.delete(`http://localhost:3333/tasks/${id}`)
+}
 
 function getEmptyTask() {
     const emptyTask: ITask = {
@@ -15,13 +55,13 @@ function getEmptyTask() {
 }
 
 function getFormData(form: HTMLFormElement, id?: number) {
-    const today = new Date();
-    const data = new FormData(form);
+    const today = new Date()
+    const data  = new FormData(form)
 
-    let completionDate = new Date(data.get('completionDate') as string);
+    let completionDate = new Date(data.get('completionDate') as string)
 
     // Adjust for the timezone offset to ensure the date is correct
-    completionDate = new Date(completionDate.getTime() + completionDate.getTimezoneOffset() * 60000);
+    completionDate = new Date(completionDate.getTime() + completionDate.getTimezoneOffset() * 60000)
 
     const formTask: ITask = {
         id: id ?? Date.now(),
@@ -32,10 +72,14 @@ function getFormData(form: HTMLFormElement, id?: number) {
         description: data.get('description') as string,
     }
 
-    return formTask;
+    return formTask
 }
 
 export const taskService = {
     getEmptyTask,
-    getFormData
+    getFormData,
+    getTasks,
+    addTask,
+    updateTask,
+    removeTask
 }
