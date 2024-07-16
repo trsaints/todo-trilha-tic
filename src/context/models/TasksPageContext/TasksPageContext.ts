32096c5dcd9fb773fbaa3917006e799dd1ffe273
trsaints <thiago.dev2k3@gmail.com>
2024-07-16 11:16:38 -0300

    update: match current context signature methods for passing its default values

--- src/context/models/TasksPageContext/TasksPageContext.ts
+++ src/context/models/TasksPageContext/TasksPageContext.ts
@@ -2,10 +2,6 @@ import {createContext} from 'react'
 import {ITasksPageContext} from './ITasksPageContext'
 
 const TasksPageContext = createContext<ITasksPageContext>({
-    saveNewTaskTemporarily: () => {},
-    createTask: () => {},
-    deleteTask: () => {},
-    updateTask: () => {},
     handleTaskOptions: () => {},
     openTaskForm: () => {}
 })
