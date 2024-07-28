b5f418fec14bb964b2fe54326bcdadc6105a0978
trsaints <thiago.dev2k3@gmail.com>
2024-07-28 10:40:47 -0300

    feat: set measurement variables for avoiding arbitrary declarations

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { App } from './components'
 
 import './reset.css'
 import './palette.css'
+import './units.css'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
